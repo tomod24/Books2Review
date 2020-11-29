34bee3ac08bc63d2f54e981045e5e3d1b0571503
tomod24 <thomo_d24@hotmail.co.uk>
2020-11-29 18:36:50 +0000

    Database updated with the correct page for the hom page. Action to pull the book infomration to the home page.

--- app.py
+++ app.py
@@ -18,11 +18,16 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
-@app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
     tasks = list(mongo.db.tasks.find())
     return render_template("tasks.html", tasks=tasks)
+
+@app.route("/")
+@app.route("/get_books")
+def get_books():
+    books = list(mongo.db.books.find())
+    return render_template("books.html", books = books)
 
 
 @app.route("/search", methods=["GET", "POST"])
@@ -117,9 +122,9 @@ def add_task():
             "due_date": request.form.get("due_date"),
             "created_by": session["user"]
         }
-        mongo.db.tasks.insert_one(task)
+        mongo.db.books.insert_one(task)
         flash("Task Successfully Added")
-        return redirect(url_for("get_tasks"))
+        return redirect(url_for("get_books"))
 
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_task.html", categories=categories)
