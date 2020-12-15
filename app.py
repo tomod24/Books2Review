453fcf2718992c03d9b9e2f7ef9a81d2baea391d
tomod24 <thomo_d24@hotmail.co.uk>
2020-12-15 23:19:07 +0000

    APp update and testing error for issues

--- app.py
+++ app.py
@@ -304,4 +304,4 @@ def error_page_not_found(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
